b7aba0879b8766ff7d6396d27091085df8ecbae3
waynedane <waynedane@outlook.com>
2018-04-15 19:12:45 +0800

    Create maxout.py

---
+++ maxout.py
@@ -0,0 +1,15 @@
+import torch.nn as nn
+class Maxout(nn.Module):
+    def __init__(self, d, m, k):
+        super(Maxout, self).__init__()
+        self.d_in, self.d_out, self.pool_size = d, m, k
+        self.lin = nn.Linear(d, m * k)
+
+    def forward(self, inputs):
+        shape = list(inputs.size())
+        shape[-1] = self.d_out
+        shape.append(self.pool_size)
+        max_dim = len(shape) - 1
+        out = self.lin(inputs)
+        m, i = out.view(*shape).max(dim=max_dim)
+        return m
